89ee22eb19d16b25dfec4b49b878b8d3eee666cd
=Erickhillo <=>
2018-08-30 12:17:04 -0500

    create payment transaction and raise

--- models/account_payment.py
+++ models/account_payment.py
@@ -3,6 +3,7 @@
 import logging
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 _logger = logging.getLogger(__name__)
 
@@ -15,23 +16,15 @@ except ImportError as err:
 
 CONEKTA_API_VERSION = "0.3.0"
 
-# test
-
-#
-#CONEKTA_PUBLIC_KEY = "key_KgVK7FVH5xRfWyaH"
-#CONEKTA_KEY="key_PqC9ZJasDD2YXzxo"
-#
-#conekta.api_key = CONEKTA_KEY
-#conekta.api_version = CONEKTA_API_VERSION
-
 class AccountPaymentConekta(models.Model):
 
     _inherit = 'account.payment'
 
     acquirer = fields.Many2one(comodel_name='payment.acquirer', string='Aquirer')
-    # cards_test_49 = fields.Selection(selection='_get_credit_cards2')
     cards_test_50 = fields.Many2one(comodel_name='conekta.credit.card', domain= lambda self:self._get_domain(),string="Conekta Credit Card")
     hide = fields.Boolean(compute='_hide_cards')
+    error = fields.Text()
+
 
     def _set_conketa_key(self):
         print 'seting up conekta key;;', self.acquirer
@@ -47,7 +40,7 @@ class AccountPaymentConekta(models.Model):
         print 'CONEKTA_KEY',CONEKTA_KEY
         conekta.api_key = CONEKTA_KEY
         conekta.api_version = CONEKTA_API_VERSION
-        
+
         return True
 
     @api.multi
@@ -64,13 +57,35 @@ class AccountPaymentConekta(models.Model):
         if self.acquirer.name == 'Conekta':
             self.hide = True
 
+    def _create_payment_transaction(self):
+        self.payment_model = self.env['payment.transaction']
+        transaction_model = {
+                'reference': self.invoice_ids.number,
+                'invoice_id': self.invoice_ids.id,
+                'amount': self.amount,
+                'currency_id': self.currency_id.id,
+                'partner_id': self.partner_id.id,
+                'acquirer_id': self.acquirer.id
+            }
+        transaction = self.payment_model.create(transaction_model)
+        return transaction
+
 
     def action_validate_invoice_payment(self):
+
         if self.acquirer.name == 'Conekta':
+            # print 'dirrrrrrrrrrr', self.id
             res = self.conekta_payment_validate()
-            if res:
-                 print 'aplica pago'
-                 values = super(AccountPaymentConekta, self).action_validate_invoice_payment()
+            # print '\n resssssssssssssssssssssssssssss',res
+            if res == True:
+                print 'aplica pago'
+                trans = self._create_payment_transaction()
+                self.payment_transaction_id = trans.id
+                values = super(AccountPaymentConekta, self).action_validate_invoice_payment()
+            else:
+                trans = self._create_payment_transaction()
+                message = 'Message form your friends at Contekta \n'+self.error
+                raise ValidationError(message)
         else:
            res = super(AccountPaymentConekta, self).action_validate_invoice_payment()
 
@@ -115,7 +130,14 @@ class AccountPaymentConekta(models.Model):
         if e:
             print e.error_json['message_to_purchaser']
             print 'NOT Charge'
+            self.error = e.error_json['message_to_purchaser']
+            print 'seeeeeeeeelllllllllllllf errorrrrrrrrrrrrrrrrrrr',self.error
             self.communication ='Not Charge'
         else:
             print 'udapte status', charge.status
             return True
+
+
+
+
+
